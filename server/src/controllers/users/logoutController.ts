a55a7548e0b2fb3d6b592441c940c5e8239e0a1a
jamsheerply <jamsheerpayyoli@gmail.com>
2024-10-18 12:21:02 +0530

    fix:cookies

--- server/src/controllers/users/logoutController.ts
+++ server/src/controllers/users/logoutController.ts
@@ -2,18 +2,11 @@ import { NextFunction, Request, Response } from "express";
 
 const logoutController = (req: Request, res: Response, next: NextFunction) => {
   try {
-    // res
-    //   .cookie("token", "", {
-    //     httpOnly: true,
-    //     secure: true,
-    //     sameSite: "strict",
-    //     expires: new Date(0),
-    //   })
     res
       .clearCookie("token", {
         httpOnly: true,
         secure: true,
-        sameSite: "strict",
+        sameSite: "none",
         path: "/",
       })
       .json({ status: true, message: "Logout successfully", user: null });
